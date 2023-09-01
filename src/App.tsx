c0891f75c12fd8e07ddc2d9a8624f73bd09f9a43
Chris Anderson <jchris@gmail.com>
2023-09-01 08:02:41 -0700

    Revert "stuff"
    This reverts commit dea3ff22d25552db3130c307dfbb7463432c0c1e.

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,28 @@
 import './App.css'
-import { fireproof, connect, useLiveQuery, useDocument } from 'use-fireproof'
-
-const otherDb = fireproof('mims')
+import { useLiveQuery, useDocument } from 'use-fireproof'
 
 function App() {
   const response = useLiveQuery('date')
   const todos = response.docs
   const [todo, setTodo, saveTodo] = useDocument({ text: '', date: Date.now(), completed: false })
-  // connect.web3(useLiveQuery.database, 'jchris+vite-test@fireproof.storage')
-  useLiveQuery.database.changes().then((changes) => {
-    // console.log(changes)
-  })
-  console.log('otherDb', otherDb)
+
   return (
     <>
-      <div>
-        <input
-          title="text"
-          type="text"
-          value={todo.text as string}
-          onChange={e => setTodo({ text: e.target.value })}
-        />
-        <button
-          onClick={e => {
-            e.preventDefault()
-            saveTodo()
-            setTodo(false)
-          }}
-        >
-          Save
-        </button>
-      </div>
+      <input
+        title="text"
+        type="text"
+        value={todo.text as string}
+        onChange={e => setTodo({ text: e.target.value })}
+      />
+      <button
+        onClick={e => {
+          e.preventDefault()
+          saveTodo()
+          setTodo(false)
+        }}
+      >
+        Save
+      </button>
       <ul>
         {todos.map(todo => (
           <li key={todo._id}>
